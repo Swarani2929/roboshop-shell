23c5eefc4e3314b8b9c9a29220b82e5bb367e226
HP <swaranigupthapulluri@gmail.com>
2022-12-07 20:52:12 +0530

    Mongodb shellscript

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 
 yum install -y mongodb-org
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl restart mongod
