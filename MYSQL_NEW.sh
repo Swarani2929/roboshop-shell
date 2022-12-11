e05c6fbf97cd5fcb5c13ae0a426c4e9cd6fe32e9
HP <swaranigupthapulluri@gmail.com>
2022-12-11 16:54:04 +0530

    mysql shellscript

---
+++ MYSQL_NEW.sh
@@ -0,0 +1,40 @@
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+
+if [ $? -eq 0 ]; then
+  echo Success
+  else
+  echo Failure
+  exit
+fi
+
+dnf module disable mysql -y
+
+if [ $? -eq 0 ]; then
+  echo Success
+  else
+  echo Failure
+  exit
+fi
+
+yum install mysql-community-server -y
+
+if [ $? -eq 0 ]; then
+  echo Success
+  else
+  echo Failure
+  exit
+fi
+
+sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}'
+if [ $? -eq 0 ]; then
+  echo Success
+  else
+  echo Failure
+  exit
+fi
+
+
+
+# systemctl enable mysqld
+# systemctl start mysqld
+
--- cart.sh
+++ cart.sh
@@ -1,7 +1,7 @@
-# curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-# yum install nodejs -y
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
 
-# useradd roboshop
+useradd roboshop
 
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 cd /home/roboshop
