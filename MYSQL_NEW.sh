804816bb4f0131c5e8b38e634d747cb2bf6e3c20
HP <swaranigupthapulluri@gmail.com>
2022-12-13 19:33:04 +0530

    updating roboshop-shell

--- MYSQL_NEW.sh
+++ MYSQL_NEW.sh
@@ -1,3 +1,8 @@
+if [-z "$1"]; then
+
+  echo Input argument password is needed
+  exit
+fi
 STAT() {
   if [ $? -eq 0 ]; then
     echo Success
@@ -9,23 +14,34 @@ STAT() {
 PRINT() {
   echo -e "\e[33m$1\e[0m"
 }
-
+PRINT "Downloading MYSQL Repo files"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 STAT $?
 
+PRINT "Disable MYSQL 8 version"
 dnf module disable mysql -y
 STAT $?
 
+PRINT "Install MYSQL"
 yum install mysql-community-server -y
 STAT $?
 
-sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}'
-STAT $?
 
+#sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}'
+#STAT $?
+
+PRINT "Enabling MYSQL"
 systemctl enable mysqld
 STAT $?
 
+PRINT "Restart MYSQL"
 systemctl restart mysqld
 STAT $?
 
+echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 
+if [$? -ne 0 ]
+then
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
+  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
