6be0ba3177478e46e5442a522c39fb63143f7cb8
HP <swaranigupthapulluri@gmail.com>
2022-12-13 14:57:10 +0530

    updating roboshop-shell

--- MYSQL_NEW.sh
+++ MYSQL_NEW.sh
@@ -1,11 +1,16 @@
+STAT() {
+  if [ $? -eq 0 ]; then
+    echo Success
+    else
+    echo Failure
+    exit
+  fi
+
+}
+
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 
-if [ $? -eq 0 ]; then
-  echo Success
-  else
-  echo Failure
-  exit
-fi
+STAT $?
 
 dnf module disable mysql -y
 
