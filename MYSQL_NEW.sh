7fb7a2d19030ea23b35e94126282a53ee77b0748
HP <swaranigupthapulluri@gmail.com>
2022-12-13 19:51:04 +0530

    updating roboshop-shell

--- MYSQL_NEW.sh
+++ MYSQL_NEW.sh
@@ -1,8 +1,6 @@
 source common.sh
 
-if [-z "$1"]
-
-then
+if [ -z "$1" ]; then
   echo Input argument password is needed
   exit
 fi
@@ -33,7 +31,7 @@ STAT $?
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 
-if [$? -ne 0 ]
+if [$? -ne 0 ];
 then
   echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
   DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
