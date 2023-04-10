e961e089e9e6aba0464c1957dc12b4619068a527
HP <swaranigupthapulluri@gmail.com>
2023-04-10 20:36:19 +0530

    First commit

--- frontend.sh
+++ frontend.sh
@@ -10,6 +10,5 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-vim /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
