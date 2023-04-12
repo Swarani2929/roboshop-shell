9c49b65d3705a894a585189d61eece653cc92ad1
HP <swaranigupthapulluri@gmail.com>
2023-04-12 19:58:12 +0530

    workstation and Route53 commits

--- frontend.sh
+++ frontend.sh
@@ -10,23 +10,23 @@ print_head(){
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
 
-echo -e "\e[35mRemoving Old content\e[0m"
+print_head "Removing Old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-echo -e "\e[35mDownloading frontend content\e[0m"
+print_head "Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-echo -e "\e[35mExtracting Downloaded Frontend Content\e[0m"
+print_head "Extracting Downloaded Frontend Content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35mCopying nginx Config for Roboshop\e[0m"
+print_head "Copying nginx Config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.config /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-echo -e "\e[35mEnabling nginx\e[0m"
+print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
 
-echo -e "\e[35mStarting nginx\e[0m"
+print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
 
 
