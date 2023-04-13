f63691d6d17dc88c2d2c55238ea31ea49c18f82c
HP <swaranigupthapulluri@gmail.com>
2023-04-13 19:35:27 +0530

    updating ./roboshop-shell

--- redis.sh
+++ redis.sh
@@ -5,11 +5,11 @@ yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_
 status_check $?
 
 print_head "Enabling 6.2 Redis Repo"
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y &>>${log_file}
 status_check $?
 
 print_head "Installing Redis"
-yum install redis -y
+yum install redis -y &>>${log_file}
 status_check $?
 
 print_head "Updating the Redis listen IP address"
@@ -17,9 +17,9 @@ sed -i -e '/s/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log
 status_check $?
 
 print_head "Enabling Redis"
-systemctl enable redis
+systemctl enable redis &>>${log_file}
 status_check $?
 
 print_head "Start Redis"
-systemctl restart redis
+systemctl restart redis &>>${log_file}
 status_check $?
