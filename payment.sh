d4e1809c23bf387430cbac9e4e49cb43d550a9dd
HP <swaranigupthapulluri@gmail.com>
2023-04-14 22:30:12 +0530

    updating ./roboshop-shell

--- payment.sh
+++ payment.sh
@@ -1,4 +1,8 @@
 source common.sh
 component=payment
-
+Roboshop_app_password=$1
+if [ -z "${Roboshop_app_password}" ]; then
+  echo -e "\e[31mMissing Roboshop app Password argument\e[0m"
+  exit 1
+fi
 python
