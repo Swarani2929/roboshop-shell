337a34d7239323f5439aa4d4377fe468951f86a1
HP <swaranigupthapulluri@gmail.com>
2023-04-14 22:11:49 +0530

    updating ./roboshop-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -7,29 +7,32 @@ if [ -z "${Roboshop_app_password}" ]; then
   exit 1
 fi
 print_head "Setup Erlang Repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>{log_fine}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>{log_file}
 status_check $?
 
 print_head "Setup Rabbitmq repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>{log_fine}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>{log_file}
 status_check $?
 
 print_head "Installing Rabbitmq"
-yum install rabbitmq-server erlang -y &>>{log_fine}
+yum install rabbitmq-server erlang -y &>>{log_file}
 status_check $?
 
 print_head "Start Rabbitmq service"
-systemctl enable rabbitmq-server &>>{log_fine}
+systemctl enable rabbitmq-server &>>{log_file}
 status_check $?
 
 print_head "Start rabbitmq service"
-systemctl start rabbitmq-server &>>{log_fine}
+systemctl start rabbitmq-server &>>{log_file}
 status_check $?
 
 print_head "Add Application user"
-rabbitmqctl add_user roboshop ${Roboshop_app_password} &>>{log_fine}
+rabbitmqctl list_users | grep roboshop &>>{log_file}
+if [ $? -ne 0 ]; then
+ rabbitmqctl add_user roboshop ${Roboshop_app_password} &>>{log_file}
+fi
 status_check $?
 
 print_head "configure set_permissions"
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>{log_fine}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>{log_file}
 status_check $?
