e7fd317fd5d518dda6753d8ffc35b533d4f0dbcf
HP <swaranigupthapulluri@gmail.com>
2023-04-14 17:05:47 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -16,3 +16,73 @@ else
  exit 1
 fi
 }
+
+NODEJS() {
+
+  print_head " Configure the Repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Install Node.js"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  print_head "Create Roboshop User"
+  id roboshop &>>{log_file}
+  if [$? -ne 0]; then
+   useradd roboshop &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Create Application directory"
+  if [ ! -d /app ]; then
+    mkdir /app &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Delete Old content"
+  rm -rf /app/*  &>>${log_file}
+  status_check $?
+
+  print_head "Downloading App Content"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  status_check $?
+
+  cd /app
+
+  print_head "Extracting App Content"
+  unzip /tmp/{component}.zip &>>${log_file}
+  status_check $?
+
+  print_head "Installing Nodejs dependencies"
+  npm install &>>${log_file}
+  status_check $?
+
+  print_head "Copy systemd service file"
+  cp ${code_dir}/configs/{component}.service /etc/systemd/system/{component}.service &>>${log_file}
+  status_check $?
+
+  print_head "Reload systemd"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+
+  print_head "Enable {component} Service"
+  systemctl enable {component} &>>${log_file}
+  status_check $?
+
+  print_head "Enable {component} Service"
+  systemctl restart {component} &>>${log_file}
+  status_check $?
+
+  print_head "Copy mongodb repo"
+  cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+  status_check $?
+
+  print_head "Install mongo client"
+  yum install mongodb-org-shell -y &>>${log_file}
+  status_check $?
+
+  print_head "Load Schema"
+  mongo --host mongodb.devops25.online </app/schema/{component}.js &>>${log_file}
+  status_check $?
+}
