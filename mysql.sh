2ecacd6c0173c72d1f0754d39b54eebdad6f3205
HP <swaranigupthapulluri@gmail.com>
2023-04-14 18:10:17 +0530

    updating ./roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -2,6 +2,10 @@ source common.sh
 
 dnf module disable mysql -y
 
+print_head "setup Mysql Repo"
+cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
+status_check $?
+
 yum install mysql-community-server -y
 
 mysql_secure_installation --set-root-pass RoboShop@1
